826cc49aefcce212568ee3058535c824d98d0d12
ajay812998 <ajay812998@gmail.com>
2020-10-16 23:40:14 +0530

    Added : Enter all Email Samples

--- userprob.sh
+++ userprob.sh
@@ -6,6 +6,7 @@ read -p "Enter your Last Name : " Lname
 read -p "Enter your Email Address : " email
 read -p "Enter your mobile Number : " mob
 read -p "Enter your 8-digit password : " password
+read -p "Enter your Email : " email1
 
 pat1="^[A-Z][a-z]{2,}$";
 pat2="^[A-Z][a-z]{2,}$";
@@ -15,8 +16,9 @@ pat5="^[a-zA-Z0-9]{8,}$";
 pat6="^(?=.*[A-Z]).{1,}(?=.*[a-zA-Z]).{7,}$";
 pat7="^(?=.*[A-Z]).{1,}(?=.*[a-zA-Z]).{7,}(?=.*[0-9]).{1,}$";
 pat8="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%+]).{8,}$";
+pat9="^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$";
 
-if [[ $password =~ $pat8 ]]
+if [[ $email1 =~ $pat9 ]]
 then
         echo "yes";
 else
