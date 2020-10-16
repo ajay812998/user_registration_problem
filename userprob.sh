5b6237f2dc704888ac83639484fe3053e4f13558
ajay812998 <ajay812998@gmail.com>
2020-10-16 22:10:36 +0530

    Added : Correct First Name

--- userprob.sh
+++ userprob.sh
@@ -1,1 +1,13 @@
-#Welcome to User Registration Problem
+#!/bin/bash -x
+
+shopt -s extglob
+read -p "Enter your first Name : " name
+
+pat="^[A-Z][a-z]{2,}$";
+
+if [[ $name =~ $pat ]]
+then
+        echo "yes";
+else
+        echo "no";
+fi
