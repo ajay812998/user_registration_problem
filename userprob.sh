c3a94696a870b2601296d275ed2176dff92e6cce
ajay812998 <ajay812998@gmail.com>
2020-10-16 22:28:51 +0530

    Added : Enter Valid Email

--- userprob.sh
+++ userprob.sh
@@ -3,11 +3,13 @@
 shopt -s extglob
 read -p "Enter your first Name : " name
 read -p "Enter your Last Name : " Lname
+read -p "Enter your Email Address : " email
 
 pat1="^[A-Z][a-z]{2,}$";
 pat2="^[A-Z][a-z]{2,}$";
+pat3="^[a-zA-Z0-9]+([.+-_][a-z-A-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,3}([.][a-z]{2})*$";
 
-if [[ $Lname =~ $pat2 ]]
+if [[ $email =~ $pat3 ]]
 then
         echo "yes";
 else
